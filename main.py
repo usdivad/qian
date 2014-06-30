4ef3118edd80bb28b83907a56f8a8caefa43e1c6
David Su <david.d.su@gmail.com>
2014-06-30 14:53:20 -0400

    calc avg monthly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import re
 import sys
 
+#Initial prompt
+# putty = raw_input("Enter 'all', 'range', or 'month':\n")
+# start_date = []
+# end_date = []
+
 #Cmd-line args
 #e.g. for tour expenses go "python main.py 3/7 3/20"
 start_date = [int(i) for i in sys.argv[1].split("/")]
@@ -13,10 +18,10 @@ data = open("log.txt", "r")
 #Regexes for params
 p_date = re.compile("\d{1,2}/\d{1,2}$")
 p_expenditure = re.compile("^\d+(\.\d+)*(?=\s)")
-p_alcohol = re.compile("beer|wine|whiskey|vodka|gin|rum|tequila|car bomb|cocktail|coca|shot")
-p_food = re.compile("breakfast|lunch|dinner|fruit")
-p_rent = re.compile("rent|hotel|motel")
-p_travel = re.compile("taxi|cab")
+p_alcohol = re.compile("beer|wine|whiskey|vodka|gin|rum|tequila|car bomb|cocktail|coca|shot|margarita|drink|anchor")
+p_food = re.compile("breakfast|brunch|lunch|dinner|fruit|food|peanut|ribs|egg|cheese|juice|apple|banana|orange|snack|gyro|pizza|avocado|cream|water|burger|Takoyaki|cafe|smoothie|taco|watermelon|turkey|enchilada|gyro|chicken|lamb|beef|Chinese|Indian|fries")
+p_rent = re.compile("rent|hotel|motel|deposit")
+p_travel = re.compile("taxi|cab|metrocard|sub|transit")
 p_entertainment = re.compile("concert|show|ticket")
 
 #Counting vars
@@ -27,17 +32,20 @@ rent = 0
 other = 0
 travel = 0
 entertainment = 0
+num_days = 0
+num_months = 1
 
 #Parse lines
 begin_counting = False
 for line in data:
     if re.search(p_date, line):
+        num_days += 1
         date = [int(i) for i in re.search(p_date, line).group().split("/")]
         #begin_counting = in_date_range(date, start_date, end_date)
         if begin_counting == False:
             if (date[0] == end_date[0] and date[1] <= end_date[1]) or (date[0] < end_date[0]):
                 begin_counting = True
-                print "huzzah! " + line
+                print "hooray! " + line
         else:
             if (date[0] == start_date[0] and date[1] <= start_date[1]) or (date[0] < start_date[0]):
                 begin_counting = False
@@ -62,6 +70,9 @@ for line in data:
                     entertainment += amt
                 else:
                     other += amt
+                    #print line
+
+num_months = num_days/30.5
 
 print "total: " + str(expenditure)
 print "food: " + str(food)
@@ -70,6 +81,7 @@ print "rent: " + str(rent)
 print "travel: " + str(travel)
 print "entertainment: " + str(entertainment)
 print "other: " + str(other)
+print "avg monthly (" + str(num_days) + " days over 30.5 days/month is " + str(num_months) + " months): " + str(expenditure/num_months)
 
 
 

ed30114f7ebfe8d87c6ecf31dcee9f0449077894
David Su <david.d.su@gmail.com>
2014-08-20 17:55:37 -0400

    add cats

--- thyme.py
+++ thyme.py
@@ -20,7 +20,7 @@ def time_diff(x, y):
         lower = x
 
     if upper - lower < 60:
-        print str(upper-lower) + ' minutes'
+        #print str(upper-lower) + ' minutes'
         return upper - lower
     else:
         upper_mins = upper%100
@@ -30,7 +30,7 @@ def time_diff(x, y):
 
         mins = upper_mins + (60-lower_mins)
         hrs = ((upper_hrs-lower_hrs)/100) - 1
-        print '{} hours and {} minutes'.format(str(hrs), str(mins))
+        #print '{} hours and {} minutes'.format(str(hrs), str(mins))
 
         return hrs*60 + mins
 
@@ -45,16 +45,56 @@ end_date = [int(i) for i in sys.argv[2].split("/")]
 #print start_date + "-" + end_date
 
 #Regexes for params
-p_date = re.compile("\d{1,2}/\d{1,2}$")
+p_date = re.compile('\d{1,2}/\d{1,2}$')
+p_timemark = re.compile('^\d+(\.\d+)*(?=\s)')
+
+p_music = re.compile('music')
+p_listen = re.compile('listen|watch|teach|lesson') #consume
+p_rehearsal = re.compile('rehearsal|jam') #public making music but not for audience
+p_performance = re.compile('performance|show') #public playing for an audience
+p_write = re.compile('write|create') #private composing, writing
+p_practice = re.compile('practice') #private practice
+p_studio = re.compile('studio')
+
+p_social = re.compile('social')
+p_errands = re.compile('errand')
+p_wash = re.compile('shower|brush|wash')
+p_travel = re.compile('travel')
+p_work = re.compile('work')
+p_idle = re.compile('idle|watch|isle')
+p_sleep = re.compile('sleep|nap')
+p_eat = re.compile('eat|dinner|breakfast|lunch')
 
 #Counting vars
 num_days = 0
 num_months = 1
+time_x = 1100 #first entry
+time_y = time_x
+
+music_amt = 0
+listen_amt = 0
+rehearsal_amt = 0
+performance_amt = 0
+write_amt = 0
+practice_amt = 0
+studio_amt = 0
+
+social_amt = 0
+errands_amt = 0
+wash_amt = 0
+travel_amt = 0
+work_amt = 0
+idle_amt = 0
+sleep_amt = 0
+eat_amt = 0
+other_amt = 0
 
 #Parse lines
-data = open('log.txt', 'r')
+data = open('log_thyme.txt', 'r')
 begin_counting = False
+
 for line in data:
+    #it's a dateline
     if re.search(p_date, line):
         num_days += 1
         date = [int(i) for i in re.search(p_date, line).group().split("/")]
@@ -68,41 +108,71 @@ for line in data:
                 begin_counting = False
                 print "we're done " + line + " "
                 break
+
+    #it's a content line
     else:
         if begin_counting == True:
-            match = re.search(p_expenditure, line) #equiv to match = p_expenditure.search(line)
+            match = re.search(p_timemark, line) #equiv to match = p_timemark.search(line)
             if match != None:
-                amt = float(match.group()) #default group(0)
-                expenditure += amt
-                if re.search(p_food, line):
-                    food += amt
-                    continue
-                elif re.search(p_alcohol, line):
-                    if not re.search(re.compile("drum|wine glasses"), line):
-                        alcohol += amt
-                        print line
+                time_new = float(match.group()) #default group(0)
+                time_x = time_y
+                time_y = time_new
+
+                amt = time_diff(time_x, time_y)
+
+                if re.search(p_music, line):
+                    music_amt += amt
+                    if re.search(p_listen, line):
+                        listen_amt += amt
                         continue
-                elif re.search(p_rent, line):
-                    rent += amt
+                    elif re.search(p_rehearsal, line):
+                        rehearsal_amt += amt
+                        continue
+                    elif re.search(p_performance, line):
+                        performance_amt += amt
+                        continue
+                    elif re.search(p_write, line):
+                        write_amt += amt
+                        continue
+                    elif re.search(p_practice, line):
+                        practice_amt += amt
+                        continue
+                    elif re.search(p_studio, line):
+                        studio_amt += amt
+                        continue
+                    else:
+                        print line
+                    continue
+                elif re.search(p_social, line):
+                    social_amt += amt
+                    continue
+                elif re.search(p_errands, line):
+                    errands_amt += amt
+                    continue
+                elif re.search(p_wash, line):
+                    wash_amt += amt
                     continue
                 elif re.search(p_travel, line):
-                    travel += amt
+                    travel_amt += amt
                     continue
-                elif re.search(p_entertainment, line):
-                    entertainment += amt
+                elif re.search(p_work, line):
+                    work_amt += amt
                     continue
-                elif re.search(p_home, line):
-                    home += amt
+                elif re.search(p_idle, line):
+                    idle_amt += amt
+                    continue
+                elif re.search(p_sleep, line):
+                    sleep_amt += amt
+                    continue
+                elif re.search(p_eat, line):
+                    eat_amt += amt
                     continue
                 else:
-                    other += amt
+                    other_amt += amt
+                    #print line
                     continue
-                    #print line
-            else:
-                ematch = re.search(p_earnings, line)
-                if ematch != None:
-                    amt = float(ematch.group().replace('+', ''))
-                    earnings += amt
-                    #print line
+
+for amt in [music_amt, social_amt, errands_amt]:
+    print ''
 
 num_months = num_days/30.5
